3bae13eddbf531f141f0f98a9f2c1df60dc5cb51
Aman <amankr97111@gmail.com>
2022-08-05 17:07:32 +0530

    first commit

---
+++ GenericsDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    internal class Program
+    {
+
+        
+       public static void Toprint(double[] inputarray)
+        {
+            foreach (double element in inputarray)
+            {
+                Console.WriteLine(element);
+            }
+        Console.WriteLine("--------------------");
+        }
+
+        public static void Toprint(char[] inputarray)
+        {
+            foreach(char element in inputarray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-------------------------");
+
+        }
+        static void Main(string[] args)
+        {
+            double[]IntArray = {1.2,1.3,1.4,1.6};
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+
+          
+            Program.Toprint(IntArray);
+            Program.Toprint(charArray);
+
+        }
+
+    }
+}
