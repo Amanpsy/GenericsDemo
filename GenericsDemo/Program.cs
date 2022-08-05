42f2b911befcb404a5d053151304d3bd398cef44
Aman <amankr97111@gmail.com>
2022-08-05 23:09:22 +0530

    implementation of generic class

--- GenericsDemo/Program.cs
+++ GenericsDemo/Program.cs
@@ -6,36 +6,51 @@ using System.Threading.Tasks;
 
 namespace GenericsDemo
 {
-    internal class Program
+    public class PrintArray<T>
     {
-        public static void Toprint<T>(T[] inputarray)
+        public T[] inputArray;
+        public PrintArray(T[] inputArray)
         {
-            foreach( var element in inputarray )
+            this.inputArray = inputArray;
+        }
+        public void toDisplay()
+        {
+            foreach (var element in inputArray)
             {
                 Console.WriteLine(element);
-            
             }
-            Console.WriteLine("------------------");
+            Console.WriteLine("------------------------");
 
         }
+
+
+    }
+    public class program
+    {
+         public static void Main(string[] args)
+        {
+            double[] doubleArray = { 1.2, 1.3, 1.4, 1.5, 1.6 };
+            char[] charArray = { 'a', 'b', 'c', 'd', 'e' };
+            int[] intArray = { 0, 1, 2, 3, 4, 5, };
+
+
+            new PrintArray<int>(intArray).toDisplay();
+            new PrintArray<char>(charArray).toDisplay();
+            new PrintArray<double>(doubleArray).toDisplay();
+
+        } 
+    }
+    
 
 
    
-       
-        static void Main(string[] args)
-        {
-            double[]DoubleArray = {1.2,1.3,1.4,1.6};
-            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
-            int[] intArray = { 0, 1, 2, 3, 4 };          
-           
-
-        Program.Toprint<int>(intArray);
-        Program.Toprint<Char>(charArray);
-        Program.Toprint<Double>(DoubleArray);
 
 
 
-        }
 
-    }
+
+
+
+
 }
+
